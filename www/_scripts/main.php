abf15d9bcb38bdf6b1ad4cf320010b3e3d712596
Gustaf Mossakowski <gustaf@koenige.org>
2023-06-03 11:54:24 +0200

    add .htaccess/main.php

---
+++ www/_scripts/main.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+ * Zugzwang Project
+ * Main script sending the requests to the Content Management System
+ *
+ * http://www.zugzwang.org/projects/zzproject
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2007-2012, 2019, 2023 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/../_inc/modules/zzwrap/zzwrap/zzwrap.php';
+zzwrap();
